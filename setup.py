22e4609ea8d43202ed3ee2b299f18fd91fa348b3
Hannes Schmidt <hannes@ucsc.edu>
2018-07-16 21:06:33 -0700

    Version 1.12.0+1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
 
 setup(
     name='chalice',
-    version='1.12.0',
+    version='1.12.0+1',
     description="Microframework",
     long_description=README,
     author="James Saryerwinnie",
